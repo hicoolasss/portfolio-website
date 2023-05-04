a8fb8205f849b3c893587df571ed3b67a078d26f
criops <criops@criops>
2023-05-04 13:40:45 +0300

    trying to cvhange donate urls

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -17,7 +17,7 @@ const sumfetch = async (args: string[]): Promise<string> => {
     ─▀███████████▒▒▒▒█▓▓▓███████████▀        <u><a href="https://linktr.ee/${config.social.linkedin}" target="_blank">${config.social.linkedin}</a></u>
     ────██▒▒▒▒▒▒▒▒▒▒▒▒█▓▓▓▓▓▓▓▓▓▓▓▓█        -----------
     ─────██▒▒▒▒▒▒▒▒▒▒▒▒██▓▓▓▓▓▓▓▓▓▓█         DONATE
-    ──────█████▒▒▒▒▒▒▒▒▒▒██████████          <u><a href="${config.donate_urls.paypal}" target="_blank">${config.donate_urls.paypal}</a></u>
+    ──────█████▒▒▒▒▒▒▒▒▒▒██████████          <u><a href="${config.donate_urls.mono}" target="_blank">${config.donate_urls.mono}</a></u>
     ─────────▀███████████▀                   <u><a href="${config.donate_urls.patreon}" target="_blank">${config.donate_urls.patreon}</a></u>
 
 `;
