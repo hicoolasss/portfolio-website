8f3bbe9efd5695e9e1071eb094ed973d60fe6f7c
criops <criops@criops>
2023-05-04 13:24:27 +0300

    changed cviient to bird ascii

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -1,24 +1,24 @@
 import config from '../../../config.json';
 
 const sumfetch = async (args: string[]): Promise<string> => {
-  if (config.ascii === 'cveinnt') {
+  if (config.ascii === 'bird') {
     return `                                                  
-             @@@@@@@@@@@@@                   sumfetch: Serikov Ilya
-        @@@@               @@@@             -----------
-      @@                       @@            ABOUT
-    @@                           @@          ${config.name}
-  @@                               @@       ﰩ ${config.ps1_hostname}
- @@                         @@@     @@       <u><a href="${config.resume_url}" target="_blank">resume</a></u>
-@@        @@@                        @@     爵 <u><a href="${config.repo}" target="_blank">github repo</a></u>
-@@                                   @@     -----------
-@@             .@@@@@@@@@@.          @@      CONTACT 
- @@           @@          @@        @@       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
-  @@           @@        @@        @@        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-   @@             @@@@@@          @@         <u><a href="https://linktr.ee/${config.social.linkedin}" target="_blank">${config.social.linkedin}</a></u>
-     @@@                        @@@         -----------
-        @@@                  @@@ @@         
-         @|  @@@@@@@@@@@@@@@@   @@          
-         @|                      @@         
+    ───────────▄██████████████▄              sumfetch: summary display
+    ───────▄████░░░░░░░░█▀────█▄            -----------
+    ──────██░░░░░░░░░░░█▀──────█▄            ABOUT
+    ─────██░░░░░░░░░░░█▀────────█▄           ${config.name}
+    ────██░░░░░░░░░░░░█──────────██         ﰩ ${config.ps1_hostname}
+    ───██░░░░░░░░░░░░░█──────██──██         ﰩ ${config.ps1_hostname}
+    ──██░░░░░░░░░░░░░░█▄─────██──██          <u><a href="${config.resume_url}" target="_blank">resume</a></u>
+    ─████████████░░░░░░██────────██         爵 <u><a href="${config.repo}" target="_blank">github repo</a></u>
+    ██░░░░░░░░░░░██░░░░░█████████████       -----------
+    ██░░░░░░░░░░░██░░░░█▓▓▓▓▓▓▓▓▓▓▓▓▓█       CONTACT
+    ██░░░░░░░░░░░██░░░█▓▓▓▓▓▓▓▓▓▓▓▓▓▓█       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+    ─▀███████████▒▒▒▒█▓▓▓███████████▀        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+    ────██▒▒▒▒▒▒▒▒▒▒▒▒█▓▓▓▓▓▓▓▓▓▓▓▓█         <u><a href="https://linktr.ee/${config.social.linkedin}" target="_blank">${config.social.linkedin}</a></u>
+    ─────██▒▒▒▒▒▒▒▒▒▒▒▒██▓▓▓▓▓▓▓▓▓▓█        -----------
+    ──────█████▒▒▒▒▒▒▒▒▒▒██████████
+    ─────────▀███████████▀      
 
 `;
   } else {
@@ -44,3 +44,21 @@ const sumfetch = async (args: string[]): Promise<string> => {
 };
 
 export default sumfetch;
+
+
+//              @@@@@@@@@@@@@                   sumfetch: summary display
+//         @@@@               @@@@             -----------
+//       @@                       @@            ABOUT
+//     @@                           @@          ${config.name}
+//   @@                               @@       ﰩ ${config.ps1_hostname}
+//  @@                         @@@     @@       <u><a href="${config.resume_url}" target="_blank">resume</a></u>
+// @@        @@@                        @@     爵 <u><a href="${config.repo}" target="_blank">github repo</a></u>
+// @@                                   @@     -----------
+// @@             .@@@@@@@@@@.          @@      CONTACT 
+//  @@           @@          @@        @@       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+//   @@           @@        @@        @@        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+//    @@             @@@@@@          @@         <u><a href="https://linktr.ee/${config.social.linkedin}" target="_blank">${config.social.linkedin}</a></u>
+//      @@@                        @@@         -----------
+//         @@@                  @@@ @@         
+//          @|  @@@@@@@@@@@@@@@@   @@          
+//          @|                      @@ 
